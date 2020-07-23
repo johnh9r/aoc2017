e4559cafb14243bce02183d96d5277415db31341
John Hagemeister <johnh9r@posteo.net>
2020-07-23 22:34:25 +0200

    d01p2: complete solution

--- day01/lib/captcha.rb
+++ day01/lib/captcha.rb
@@ -1,5 +1,5 @@
 def sum_digit_pairs(filename)
-  digits = File.open(filename).read.chomp.split('').map(&:to_i)
+  digits = digits(filename)
 
   # duplicate first digit at very end to mimic circular nature
   digits.push(digits[0])
@@ -15,4 +15,27 @@ def sum_digit_pairs(filename)
     end
 
   sum
+end
+
+def sum_para_digit_pairs(filename)
+  digits = digits(filename)
+  num_elem = digits.length
+  fail 'even number of elements expected' if num_elem % 2 != 0
+
+  sum, _ =
+    digits.each_with_index.reduce(0) do |acc, d_i|
+      d, i = d_i
+      opp_digit = digits[(i + num_elem / 2) % num_elem]
+      if opp_digit == d
+        acc + d
+      else
+        acc
+      end
+    end
+
+  sum
+end
+
+def digits(filename)
+  File.open(filename).read.chomp.split('').map(&:to_i)
 end
--- day01/spec/captcha_spec.rb
+++ day01/spec/captcha_spec.rb
@@ -37,4 +37,34 @@ describe 'captcha' do
     end
   end
 
+  describe 'part 2' do
+    example 'ex #1', input: 1212, output: 6 do |ex|
+      f = tmp_file(ex.metadata[:input])
+      expect(sum_para_digit_pairs(f.path)).to equal(ex.metadata[:output])
+    end
+
+    example 'ex #2', input: 1221, output: 0 do |ex|
+      f = tmp_file(ex.metadata[:input])
+      expect(sum_para_digit_pairs(f.path)).to equal(ex.metadata[:output])
+    end
+
+    example 'ex #3', input: 123425, output: 4 do |ex|
+      f = tmp_file(ex.metadata[:input])
+      expect(sum_para_digit_pairs(f.path)).to equal(ex.metadata[:output])
+    end
+
+    example 'ex #4', input: 123123, output: 12 do |ex|
+      f = tmp_file(ex.metadata[:input])
+      expect(sum_para_digit_pairs(f.path)).to equal(ex.metadata[:output])
+    end
+
+    example 'ex #5', input: 12131515, output: 4 do |ex|
+      f = tmp_file(ex.metadata[:input])
+      expect(sum_para_digit_pairs(f.path)).to equal(ex.metadata[:output])
+    end
+
+    it 'sums identical digits in opposing position in circular input' do
+      expect(sum_para_digit_pairs('./spec/fixtures/input-part1.txt')).to equal(1152)
+    end
+  end
 end
