f7dad5df6dcaba2f2ac7359376b25d0e75528a4f
John Hagemeister <johnh9r@posteo.net>
2020-07-25 21:13:45 +0200

    d02p2: complete solution

--- day02/lib/checksum.rb
+++ day02/lib/checksum.rb
@@ -17,6 +17,29 @@ def calc_checksum(input)
   min_max_rs.sum { |elem| elem[1] - elem[0] }
 end
 
+def calc_checksum_quotients(input)
+  all_rows = int_rows(input)
+
+  quot_div_rs =
+    all_rows.reduce([]) do |acc_rs, row|
+
+      quot_div =
+        row.reduce([nil, nil]) do |acc, v|
+          elems = row.drop_while { |x| (x == v) || ((x % v != 0) && (v % x != 0)) }
+          if elems.empty?
+            acc
+          else
+            x = elems[0]
+            (v < x) ? [v, x] : [x, v]
+          end
+        end
+
+      acc_rs << quot_div
+    end
+
+  quot_div_rs.sum { |elem| elem[1] / elem[0] }
+end
+
 def int_rows(input)
   all_rows = input.split("\n")
   all_rows.map do |row|
--- day02/spec/checksum_spec.rb
+++ day02/spec/checksum_spec.rb
@@ -10,3 +10,14 @@ describe 'part 1' do
     expect(calc_checksum(input)).to equal(44_216)
   end
 end
+
+describe 'part 2' do
+  example 'ex #1', input: "5 9 2 8\n9 4 7 3\n3 8 6 5\n", output: 9 do |ex|
+    expect(calc_checksum_quotients(ex.metadata[:input])).to equal(ex.metadata[:output])
+  end
+
+  it 'calculates sum of integer quotients per row for spreadsheet' do
+    input = File.open('./spec/fixtures/input-part1.txt').read.chomp
+    expect(calc_checksum_quotients(input)).to equal(320)
+  end
+end
